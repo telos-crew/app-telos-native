d94f1acc8fe83d2b20d8106024921b5f133400f2
Kylan Hurt <kylan@ridian.io>
2023-02-14 20:46:51 -0600

    Start showing Create New Item dialog

--- src/pages/wishlist/BallotFilters.vue
+++ src/pages/wishlist/BallotFilters.vue
@@ -14,20 +14,37 @@
         </template>
       </q-select>
     </div>
+    <q-btn @click="toggleDialog" color="primary" size="lg">
+      Create New
+    </q-btn>
   </div>
+  <q-dialog v-model="dialog">
+   <create-item-form />
+  </q-dialog>
 </template>
 
 <script>
+import CreateItemForm from "./components/CreateItemForm.vue";
+
 const options = [
   { label: "Highest Approval", value: "highest-approval" },
   { label: "Lowest Approval", value: "lowest-approval" }
 ];
 export default {
+  components: {
+    CreateItemForm
+  },
   data() {
     return {
       options,
       sort: null,
+      dialog: false
     };
+  },
+  methods: {
+    toggleDialog () {
+      this.dialog = !this.dialog
+    }
   },
   watch: {
     sort(newValue) {
@@ -40,6 +57,9 @@ export default {
 <style lang="scss" scoped>
 #ballot-filters {
   width: 100%;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
   background: white;
   box-shadow: 0px 20px 48px rgb(0 9 26 / 8%), 0px 7px 15px rgb(0 9 26 / 5%),
     0px 3px 6px rgb(0 9 26 / 4%), 0px 1px 2.25px rgb(0 9 26 / 4%);
---
+++ src/pages/wishlist/components/CreateItemForm.vue
@@ -0,0 +1,28 @@
+<template>
+    <q-card class="dismiss-offer-form">
+        <q-card-section>
+            <div class="text-h6">{{$t('pages.resolve.dismiss_offer_heading')}}</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+            <p>
+                {{$t('pages.resolve.dismiss_offer_text')}}
+            </p>
+        </q-card-section>
+
+        <q-card-actions align="right" class="text-primary">
+            <q-btn flat :label="$t('pages.resolve.dismiss_offer_dismiss_cta')" @click="submit" />
+            <q-btn flat :label="$t('pages.resolve.dismiss_offer_cancel_cta')" @click="close" />
+        </q-card-actions>
+    </q-card>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
