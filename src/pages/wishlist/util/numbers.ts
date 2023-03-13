15bde648822e90c103a8877b2d7787c6849254a5
Kylan Hurt <kylan@ridian.io>
2023-03-13 19:29:34 -0300

    Shorten aggregated vote numbers

---
+++ src/pages/wishlist/util/numbers.ts
@@ -0,0 +1,10 @@
+export const formatVoteCount = (count: number) => {
+	if (Math.abs(count) > 999999) {
+		return `${(count / 1000000).toFixed(1)}M`
+	}
+	if (Math.abs(count) > 999) {
+		return `${(count / 1000).toFixed(1)}K`
+	}
+
+	return count
+}
