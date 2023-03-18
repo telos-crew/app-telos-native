34957e397880fb20c480e37b3bcee00e2bad5feb
Kylan Hurt <kylan@ridian.io>
2023-03-18 13:43:53 -0300

    Apparently get nesting working

--- src/pages/wishlist/util/comments.ts
+++ src/pages/wishlist/util/comments.ts
@@ -2,36 +2,32 @@
 import { BallotComment } from '../types/blockchain';
 
 export const buildCommentTree = (comments: BallotComment[]) => {
-	let commentMap = {};
-	let level: null | number = null;
+	let iterator = 0;
+	const levels = {};
+	const commentMap = {};
 	comments.forEach((comment) => {
-		console.log(comment.id);
-		if (comment.parent_id === null) {
-			commentMap[comment.id] = {
-				...commentMap[comment.id],
-				...comment
-			};
-		} else {
-			// if parent already exists
-			if (commentMap[comment.parent_id]) {
-				commentMap[comment.parent_id] = {
-					...commentMap[comment.parent_id],
-					children: {
-						...commentMap[comment.parent_id].children,
-						[comment.id]: comment
-					}
-				};
-			} else {
-				// if parent does not exist
-				commentMap[comment.parent_id] = {
-					id: comment.parent_id,
-					children: {
-						[comment.id]: comment
-					}
-				};
-			}
+		if (!levels[comment.level]) {
+			levels[comment.level] = {};
 		}
-		console.log('commentMap', JSON.parse(JSON.stringify(commentMap)));
+		levels[comment.level][comment.id] = comment;
+	});
+	console.log('levels before', JSON.parse(JSON.stringify(levels)));
+	const sortedLevelKeys = Object.keys(levels).sort().reverse();
+	sortedLevelKeys.forEach((levelKey: string) => {
+		const levelComments = levels[levelKey];
+		if (levelKey !== '0') {
+			// sort comments?
+			const parentLevel = levels[parseInt(levelKey) - 1];
+			for (const key in levelComments) {
+				const comment = levelComments[key];
+				if (!parentLevel[comment.parent_id].children) {
+					parentLevel[comment.parent_id].children = [comment];
+				} else {
+					parentLevel[comment.parent_id].children.push(comment);
+				}
+			}
+			console.log('parentLevel final: ', parentLevel);
+		}
 	});
 };
 
