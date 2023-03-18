91316389645308f5d31f415a359af5047a7d3f4f
Kylan Hurt <kylan@ridian.io>
2023-03-18 12:19:11 -0300

    WIP 2

--- src/pages/wishlist/util/comments.ts
+++ src/pages/wishlist/util/comments.ts
@@ -5,28 +5,35 @@ export const buildCommentTree = (comments: BallotComment[]) => {
 	let commentMap = {};
 	let level: null | number = null;
 	comments.forEach((comment) => {
-		// if it is a new level
-		if (level !== comment.level) {
-			level = comment.level;
-			// if it is top-level
-			if (comment.parent_id === null) {
-				commentMap[comment.id] = {
-					...commentMap[comment.id],
-					...comment
+		console.log(comment.id);
+		if (comment.parent_id === null) {
+			commentMap[comment.id] = {
+				...commentMap[comment.id],
+				...comment
+			};
+		} else {
+			// if parent already exists
+			if (commentMap[comment.parent_id]) {
+				commentMap[comment.parent_id] = {
+					...commentMap[comment.parent_id],
+					children: {
+						...commentMap[comment.parent_id].children,
+						[comment.id]: comment
+					}
 				};
 			} else {
-				// if not top-level
+				// if parent does not exist
 				commentMap[comment.parent_id] = {
+					id: comment.parent_id,
 					children: {
 						[comment.id]: comment
-					},
-					id: comment.parent_id
+					}
 				};
 			}
-		} else {
 		}
+		console.log('commentMap', JSON.stringify(commentMap));
+		console.log('commentMap', commentMap);
 	});
-	console.log('commentMap', commentMap);
 };
 
 export const something = {
