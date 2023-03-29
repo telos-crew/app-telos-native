ec1bc550d3d297d3df63ebb23bbc5738a6057367
Kylan Hurt <kylan.hurt@gmail.com>
2023-03-29 07:32:23 -0300

    Period in router param (URL) does not break app

--- quasar.config.js
+++ quasar.config.js
@@ -139,7 +139,10 @@ module.exports = configure(function (ctx) {
 				type: 'http'
 			},
 			port: 8080,
-			open: true // opens browser window automatically
+			open: true, // opens browser window automatically
+			historyApiFallback: {
+				disableDotRule: true
+		}
 		},
 
 		// https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-framework
