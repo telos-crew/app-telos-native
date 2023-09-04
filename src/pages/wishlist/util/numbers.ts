d484448e677e71dd97a31d2f1ad3f8c2cb81f14b
Kylan Hurt <kylan.hurt@gmail.com>
2023-09-04 13:34:27 -0300

    Change negative scores to zero

--- src/pages/wishlist/util/numbers.ts
+++ src/pages/wishlist/util/numbers.ts
@@ -1,4 +1,5 @@
-export const formatVoteCount = (count: number) => {
+export const formatVoteCount = (count: number): string => {
+	if (count < 0) return '0'
 	if (Math.abs(count) > 999999) {
 		return `${(count / 1000000).toFixed(1)}M`
 	}
